3f35bdda2d0e371bb22e0a81dd7581ce4b6eab83
Victor Fernandez Robles <victor@MacBook-Air-de-Victor.local>
2022-09-08 18:00:26 +0200

    default CRUD

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TemplatesModule } from './templates/templates.module';
 
 @Module({
-  imports: [],
+  imports: [TemplatesModule],
   controllers: [AppController],
   providers: [AppService],
 })
