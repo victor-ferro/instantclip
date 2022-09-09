f567043997e6c7bf071a63d2e07f275673a1f092
Victor Fernandez Robles <victor@MacBook-Air-de-Victor.local>
2022-09-09 17:44:37 +0200

    Real Api values

--- src/templates/model/template.ts
+++ src/templates/model/template.ts
@@ -1,12 +1,35 @@
 //Data Transfer Object
 
 export class Template {
-    name: string
     id: string
+    title: string
+    thumbnailUrl: string
     creator: Creator
+    music: Music
+    authorName: string
+    authorAvatarUrl: string
+    sample: string
+    tags: string[]
+    template: Templates
 }
 
 export class Creator{
-    avatar: string
     name: string
+    username: string
+    avatarUrl: string
+}
+
+export class Music{
+    name: string
+}
+
+export class Templates{
+    duration: number
+    soundUrl: string
+    slots: Slot[]
+}
+
+export class Slot{
+    time: number
+    type: string
 }
