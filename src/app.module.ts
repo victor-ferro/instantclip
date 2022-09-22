4e4e847508a42f6440001b3fbc3fa73730da5b9e
Victor Fernandez Robles <victor@MacBook-Air-de-Victor.local>
2022-09-22 22:05:15 +0200

    add package TypeORM and mysql2. Add the TypeORM module to app.module and cofigurate properties for conexion. Inject dataSource from typeORM for have the posibility to inject dataSource and EntityManager across the entire project

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,25 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TemplatesModule } from './templates/templates.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm'
 
 @Module({
-  imports: [TemplatesModule],
+  imports: [TemplatesModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'instantclip',
+      entities: [],
+      synchronize: true //TRUE --> only in dev. Pass to false in production
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+}
