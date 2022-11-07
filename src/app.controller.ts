14f58602f9fa0c7a82e315b60995f8b6c20fd337
Victor Ferro <victorferro4@gmail.com>
2022-11-07 17:04:22 +0100

    We modified the storage of the JWT, before it was requested from headers and now from cookies. For this we use setHeader to store the token. We use in the main.ts app.use(cookieParser()) so that from the jwt.strategy we can extract it with .fromExtractors()

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,9 @@
-import { Controller, Request, Post, UseGuards, Get } from '@nestjs/common';
+import { Controller, Request, Post, UseGuards, Get, Response, Res, Body } from '@nestjs/common';
 import { AuthService } from './auth/auth.service';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { LocalAuthGuard } from './auth/local-auth.guard';
+import { Template } from './templates/model/template';
+import { UsersService } from './users/users.service';
 
 @Controller()
 export class AppController {
@@ -9,8 +11,17 @@ export class AppController {
 
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
-  async login(@Request() req) {
-    return this.authService.login(req.user);
+  async login(@Request() req, @Response() res) {
+    const cookie = this.authService.getCookieWithJwtToken(req.user);
+    res.setHeader('Set-Cookie', cookie)
+    return res.send(cookie);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Post('log-out')
+  async logOut(@Request() request, @Res() response) {
+    response.setHeader('Set-Cookie', this.authService.getCookieForLogOut());
+    return response.sendStatus(200);
   }
 
   @Get()
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt'
+import { jwtConstants } from './constants';
 
 @Injectable()
 export class AuthService {
@@ -34,5 +35,17 @@ export class AuthService {
         return {
             access_token: this.jwtService.sign(payload),
         };
+    }
+
+    getCookieWithJwtToken(user: any) {
+        const payload = {
+            username: user.username, sub: user.userId
+        };
+        const token = this.jwtService.sign(payload);
+        return `Authentication=${token}; HttpOnly; Path=/; Max-Age=${jwtConstants.timeToExpire}`;
+    }
+
+    getCookieForLogOut(){
+        return `Authentication=; HttpOnly; Path=/; Max-Age=0`;
     }
 }
